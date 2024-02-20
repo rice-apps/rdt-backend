022a78df7f2806a101301b33374cd5c5966ec15c
Anthony Yan <ady2@rice.edu>
2024-02-20 15:35:17 -0600

    restructure

---
+++ Server/models/User.js
@@ -0,0 +1,30 @@
+const mongoose = require("mongoose")
+
+const userSchema = new mongoose.Schema({
+    username: {
+        type: String,
+        required: true,
+        unique: true,
+    },
+    password: {
+        type: String,
+        required: true,
+        unique: true,
+    },
+    email: {
+        type: String,
+        required: true,
+        unique: true,
+    },
+    userType: {
+        type: String,
+        required: true,
+    },
+    venmoHandle: {
+        type: String,
+        unique: true,
+    }
+})
+
+const Item = mongoose.model('User', userSchema)
+module.exports = Item
