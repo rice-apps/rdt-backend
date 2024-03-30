da62f82d7af45015ff3d2ec7f578b6d5e7535537
quangnguyen <qnn1@rice.edu>
2024-03-30 14:56:59 -0500

    Fix CORS issue

--- index.js
+++ index.js
@@ -49,7 +49,31 @@ require("./config/google");
 const app = express();
 app.use(bodyParser.json());
 
-app.use(cors({ origin: "http://localhost:3001", credentials: true }));
+const allowedOrigins = [
+  "http://localhost:3001",
+  "http://localhost:3000",
+  "https://rdt-backend-production.up.railway.app",
+  "https://superb-pocket.surge.sh",
+  "enormous-fowl.surge.sh",
+];
+
+app.use(
+  cors({
+    origin: (origin, callback) => {
+      if (!origin) return callback(null, true);
+
+      if (allowedOrigins.indexOf(origin) === -1) {
+        const msg =
+          "The CORS policy for this site does not allow access from the specified Origin.";
+        return callback(new Error(msg), false);
+      }
+      return callback(null, true);
+    },
+    credentials: true,
+  })
+);
+
+// app.use(cors({ origin: "http://localhost:3001", credentials: true }));
 // routes
 var authRouter = require("./routes/auth");
 
